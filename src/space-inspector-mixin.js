9722ca5b90a8b7de3847a9df769820deb6d498c6
Miëtek Bak <mietek@bak.io>
2015-11-20 19:22:16 +0000

    Fix local mean painting in space inspector

--- src/space-inspector-mixin.js
+++ src/space-inspector-mixin.js
@@ -39,6 +39,7 @@ module.exports = {
   // TODO: Refactor
   paintSILocalMeans: function (c) {
     c.save();
+    c.translate(0.25, 0.25);
     c.fillStyle = defs.roadLinkColor;
     for (var x = 0; x < columnCount; x++) {
       for (var y = 0; y < rowCount; y++) {
