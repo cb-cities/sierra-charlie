02d60c31ab971659d297284a2103fb44004d4370
Miëtek Bak <mietek@bak.io>
2015-12-29 15:54:02 +0000

    Checkpoint

--- src/app.js
+++ src/app.js
@@ -87,6 +87,10 @@ module.exports = {
     this._geometryLoader.update(this.state.left, this.state.top);
     this._renderer.update(canvas, this.state.left, this.state.top, compute.time(this.state.rawTime), this.state.zoom);
     this._painter.update(canvas, this.state.left, this.state.top, compute.time(this.state.rawTime), this.state.zoom);
+
+    addEventListener("resize", this.onResize);
+    matchMedia("screen and (min-resolution: 2dppx)").addListener(this.onRescale);
+    addEventListener("keydown", this.onKeyDown);
   },
 
   componentDidUpdate: function () {
@@ -106,6 +110,21 @@ module.exports = {
     this._renderer.update(canvas, left, top, time, zoom);
     this._painter.update(canvas, left, top, time, zoom);
   },
+  
+  render: function () {
+    var zoom = this.getEasedState("zoom");
+    return (
+      r.div("map-frame",
+        r.canvas("map-canvas"),
+        r.div({
+            className: "map-space",
+            style: {
+              width:  compute.spaceWidth(zoom),
+              height: compute.spaceHeight(zoom)
+            },
+            onDoubleClick: this.onDoubleClick
+          })));
+  },
 
   onScroll: function (event) {
     if (!this.easingLeft && !this.easingTop && !this.easingZoom) {
@@ -116,6 +135,15 @@ module.exports = {
           top:  frame.scrollTop / compute.spaceHeight(zoom)
         });
     }
+  },
+  
+  onResize: function (event) {
+    this.update();
+  },
+  
+  onRescale: function (event) {
+    console.log("window.devicePixelRatio:", window.devicePixelRatio);
+    this.update();
   },
 
   onDoubleClick: function (event) {
@@ -132,20 +160,58 @@ module.exports = {
       this.setZoom(Math.min(this.state.zoom + 1, defs.maxZoom), duration);
     }
   },
-
-  render: function () {
-    var zoom = this.getEasedState("zoom");
-    return (
-      r.div("map-frame",
-        r.canvas("map-canvas"),
-        r.div({
-            className: "map-space",
-            style: {
-              width:  compute.spaceWidth(zoom),
-              height: compute.spaceHeight(zoom)
-            },
-            onDoubleClick: this.onDoubleClick
-          })));
+  
+  onKeyDown: function (event) {
+    // console.log("keyDown", event.keyCode);
+    var canvas = r.domNode(this).firstChild;
+    var pageWidth  = compute.pageWidth(canvas.clientWidth, this.state.zoom);
+    var pageHeight = compute.pageHeight(canvas.clientHeight, this.state.zoom);
+    var duration = event.shiftKey ? 2500 : 500;
+    var timeDelta = (event.ctrlKey || event.altKey) ? 60 : 3600;
+    var zoomDelta = (event.altKey || event.ctrlKey) ? 2 : 10;
+    switch (event.keyCode) {
+      case 37: // left
+      case 36: // home
+        var left = Math.max(0, this.state.left - pageWidth / (event.keyCode === 36 ? 1 : 10));
+        this.setLeft(left, duration);
+        break;
+      case 39: // right
+      case 35: // end
+        var left = Math.min(this.state.left + pageWidth / (event.keyCode === 35 ? 1 : 10), 1);
+        this.setLeft(left, duration);
+        break;
+      case 38: // up
+      case 33: // page up
+        var top = Math.max(0, this.state.top - pageHeight / (event.keyCode === 33 ? 1 : 10));
+        this.setTop(top, duration);
+        break;
+      case 40: // down
+      case 34: // page down
+        var top = Math.min(this.state.top + pageHeight / (event.keyCode === 34 ? 1 : 10), 1);
+        this.setTop(top, duration);
+        break;
+      case 219: // left bracket
+        var rawTime = Math.round((this.state.rawTime * 3600) - timeDelta) / 3600;
+        this.setRawTime(rawTime, duration);
+        break;
+      case 221: // right bracket
+        var rawTime = Math.round((this.state.rawTime * 3600) + timeDelta) / 3600;
+        this.setRawTime(rawTime, duration);
+        break;
+      case 187: // plus
+        var zoom = Math.max(0, (Math.round((this.state.zoom * 10) - zoomDelta) / 10));
+        this.setZoom(zoom, duration);
+        break;
+      case 189: // minus
+        var zoom = Math.min(Math.round((this.state.zoom * 10) + zoomDelta) / 10, defs.maxZoom);
+        this.setZoom(zoom, duration);
+        break;
+      default:
+        if (event.keyCode >= 49 && event.keyCode <= 56) {
+          var zoom = event.keyCode - 49;
+          this.setZoom(zoom, duration);
+        }
+    }
   }
 };
 
