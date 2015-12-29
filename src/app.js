4bb5a5d218c6c61e6246f2e47809271357289b9f
Miëtek Bak <mietek@bak.io>
2015-12-29 02:08:54 +0000

    Checkpoint

--- src/app.js
+++ src/app.js
@@ -9,6 +9,24 @@ var defs = require("./defs");
 var easeStateMixin = require("./ease-state-mixin");
 
 
+function computeTimeValue(rawTimeValue) {
+  return (
+    rawTimeValue >= 0 ?
+      Math.round((rawTimeValue * 3600) % (24 * 3600)) / 3600 :
+      24 - Math.round((-rawTimeValue * 3600) % (24 * 3600)) / 3600);
+}
+
+function computeTimeSlice(timeValue) {
+  return (
+    (timeValue >=  8 && timeValue < 10) ? 1 :
+    (timeValue >= 10 && timeValue < 13) ? 2 :
+    (timeValue >= 13 && timeValue < 16) ? 3 :
+    (timeValue >= 16 && timeValue < 19) ? 4 :
+    (timeValue >= 19 && timeValue < 21) ? 5 :
+    0);
+}
+
+
 module.exports = {
   mixins: [
     easeStateMixin
@@ -119,8 +137,6 @@ module.exports = {
     this.node = r.domNode(this);
     this.canvas = this.node.firstChild;
     this.node.addEventListener("scroll", this.onScroll);
-    addEventListener("resize", this.onResize);
-    addEventListener("keydown", this.onKeyDown);
     this.triggerUpdate();
     this.computeDerivedState();
     this.exportScrollPosition();
@@ -131,8 +147,6 @@ module.exports = {
 
   componentWillUnmount: function () {
     this.node.removeEventListener("scroll", this.onScroll);
-    removeEventListener("resize", this.onResize);
-    removeEventListener("keydown", this.onKeyDown);
   },
 
   triggerUpdate: function () {
@@ -155,10 +169,6 @@ module.exports = {
     }
   },
 
-  onResize: function (event) {
-    this.triggerUpdate();
-  },
-
   importScrollPosition: function () {
     this.setState({
         attentionLeft: this.node.scrollLeft / this.easedWidth,
@@ -166,31 +176,13 @@ module.exports = {
       });
   },
 
-  // TODO: Refactor
-  computeTimeValue: function (rawTimeValue) {
-    return (
-      rawTimeValue >= 0 ?
-        Math.round((rawTimeValue * 3600) % (24 * 3600)) / 3600 :
-        24 - Math.round((-rawTimeValue * 3600) % (24 * 3600)) / 3600);
-  },
-
-  computeTimeSlice: function (timeValue) {
-    return (
-      (timeValue >=  8 && timeValue < 10) ? 1 :
-      (timeValue >= 10 && timeValue < 13) ? 2 :
-      (timeValue >= 13 && timeValue < 16) ? 3 :
-      (timeValue >= 16 && timeValue < 19) ? 4 :
-      (timeValue >= 19 && timeValue < 21) ? 5 :
-      0);
-  },
-
   computeDerivedState: function () {
     this.easedAttentionLeft = this.getEasedState("attentionLeft");
     this.easedAttentionTop  = this.getEasedState("attentionTop");
-    this.easedTimeValue     = this.computeTimeValue(this.getEasedState("rawTimeValue"));
+    this.easedTimeValue     = computeTimeValue(this.getEasedState("rawTimeValue"));
     this.easedZoomPower     = this.getEasedState("zoomPower");
     this.floorTimeValue = Math.floor(this.easedTimeValue);
-    this.timeSlice      = this.computeTimeSlice(this.floorTimeValue);
+    this.timeSlice      = computeTimeSlice(this.floorTimeValue);
     this.floorZoomPower = Math.floor(this.easedZoomPower);
     this.roundZoomPower = Math.round(this.easedZoomPower);
     this.ceilZoomPower  = Math.ceil(this.easedZoomPower);
@@ -217,58 +209,6 @@ module.exports = {
   exportScrollPosition: function () {
     this.node.scrollLeft = Math.floor(this.easedAttentionLeft * this.easedWidth);
     this.node.scrollTop  = Math.floor(this.easedAttentionTop * this.easedHeight);
-  },
-
-
-  onKeyDown: function (event) {
-    // console.log("keyDown", event.keyCode);
-    var pageWidth  = 1 / (this.easedWidth / this.node.clientWidth);
-    var pageHeight = 1 / (this.easedHeight / this.node.clientHeight);
-    var delay = event.shiftKey ? 2500 : 500;
-    var timeDelta = (event.ctrlKey || event.altKey) ? 60 : 3600;
-    var zoomDelta = (event.altKey || event.ctrlKey) ? 2 : 10;
-    switch (event.keyCode) {
-      case 37: // left
-      case 36: // home
-        var left = this.state.attentionLeft - pageWidth / (event.keyCode === 36 ? 1 : 10);
-        this.easeAttentionLeft(Math.max(0, left), delay);
-        break;
-      case 39: // right
-      case 35: // end
-        var left = this.state.attentionLeft + pageWidth / (event.keyCode === 35 ? 1 : 10);
-        this.easeAttentionLeft(Math.min(left, 1), delay);
-        break;
-      case 38: // up
-      case 33: // page up
-        var top = this.state.attentionTop - pageHeight / (event.keyCode === 33 ? 1 : 10);
-        this.easeAttentionTop(Math.max(0, top), delay);
-        break;
-      case 40: // down
-      case 34: // page down
-        var top = this.state.attentionTop + pageHeight / (event.keyCode === 34 ? 1 : 10);
-        this.easeAttentionTop(Math.min(top, 1), delay);
-        break;
-      case 219: // left bracket
-        var rawTimeValue = (Math.round((this.state.rawTimeValue * 3600) - timeDelta) / 3600);
-        this.easeTimeValue(rawTimeValue, delay);
-        break;
-      case 221: // right bracket
-        var rawTimeValue = (Math.round((this.state.rawTimeValue * 3600) + timeDelta) / 3600);
-        this.easeTimeValue(rawTimeValue, delay);
-        break;
-      case 187: // plus
-        var zoomPower = Math.max(0, (Math.round((this.state.zoomPower * 10) - zoomDelta) / 10));
-        this.easeZoomPower(zoomPower, delay);
-        break;
-      case 189: // minus
-        var zoomPower = Math.min(Math.round((this.state.zoomPower * 10) + zoomDelta) / 10, defs.maxZoomPower);
-        this.easeZoomPower(zoomPower, delay);
-        break;
-      default:
-        if (event.keyCode >= 49 && event.keyCode <= 56) {
-          this.easeZoomPower(event.keyCode - 49, delay);
-        }
-    }
   },
 
   onDoubleClick: function (event) {
--- src/index.js
+++ src/index.js
@@ -12,4 +12,60 @@ require("./index.html");
 var fragmentShader = require("./shaders/fragment-shader.glsl");
 var vertexShader = require("./shaders/vertex-shader.glsl");
 
-r.render(app(), document.getElementById("root"));
+
+var that = r.render(app(), document.getElementById("root"));
+
+addEventListener("resize", function (event) {
+    this.triggerUpdate();
+  }.bind(that));
+
+addEventListener("keydown", function (event) {
+    // console.log("keyDown", event.keyCode);
+    var pageWidth  = 1 / (this.easedWidth / this.node.clientWidth);
+    var pageHeight = 1 / (this.easedHeight / this.node.clientHeight);
+    var delay = event.shiftKey ? 2500 : 500;
+    var timeDelta = (event.ctrlKey || event.altKey) ? 60 : 3600;
+    var zoomDelta = (event.altKey || event.ctrlKey) ? 2 : 10;
+    switch (event.keyCode) {
+      case 37: // left
+      case 36: // home
+        var left = this.state.attentionLeft - pageWidth / (event.keyCode === 36 ? 1 : 10);
+        this.easeAttentionLeft(Math.max(0, left), delay);
+        break;
+      case 39: // right
+      case 35: // end
+        var left = this.state.attentionLeft + pageWidth / (event.keyCode === 35 ? 1 : 10);
+        this.easeAttentionLeft(Math.min(left, 1), delay);
+        break;
+      case 38: // up
+      case 33: // page up
+        var top = this.state.attentionTop - pageHeight / (event.keyCode === 33 ? 1 : 10);
+        this.easeAttentionTop(Math.max(0, top), delay);
+        break;
+      case 40: // down
+      case 34: // page down
+        var top = this.state.attentionTop + pageHeight / (event.keyCode === 34 ? 1 : 10);
+        this.easeAttentionTop(Math.min(top, 1), delay);
+        break;
+      case 219: // left bracket
+        var rawTimeValue = (Math.round((this.state.rawTimeValue * 3600) - timeDelta) / 3600);
+        this.easeTimeValue(rawTimeValue, delay);
+        break;
+      case 221: // right bracket
+        var rawTimeValue = (Math.round((this.state.rawTimeValue * 3600) + timeDelta) / 3600);
+        this.easeTimeValue(rawTimeValue, delay);
+        break;
+      case 187: // plus
+        var zoomPower = Math.max(0, (Math.round((this.state.zoomPower * 10) - zoomDelta) / 10));
+        this.easeZoomPower(zoomPower, delay);
+        break;
+      case 189: // minus
+        var zoomPower = Math.min(Math.round((this.state.zoomPower * 10) + zoomDelta) / 10, defs.maxZoomPower);
+        this.easeZoomPower(zoomPower, delay);
+        break;
+      default:
+        if (event.keyCode >= 49 && event.keyCode <= 56) {
+          this.easeZoomPower(event.keyCode - 49, delay);
+        }
+    }
+  }.bind(that));
