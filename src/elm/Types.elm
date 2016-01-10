0d58f09dcaf0d513a9f36ce284c7993ec8250c5a
Miëtek Bak <mietek@bak.io>
2016-01-10 05:48:27 +0000

    Restructure UI code

---
+++ src/elm/Types.elm
@@ -0,0 +1,57 @@
+module Types where
+
+import Signal
+
+
+type alias RoadNode =
+  { toid : String
+  , address : Maybe String
+  , roadLinks : List String
+  }
+
+
+type alias RoadLink =
+  { toid : String
+  , term : String
+  , nature : String
+  , negativeNode : Maybe String
+  , positiveNode : Maybe String
+  , roads : List Road
+  }
+
+
+type alias Road =
+  { toid : String
+  , group : String
+  , term : Maybe String
+  , name : String
+  , roadLinks : List String
+  }
+
+
+type alias Feature =
+  { tag : String
+  , roadNode : Maybe RoadNode
+  , roadLink : Maybe RoadLink
+  , road : Maybe Road
+  }
+
+
+type alias Model =
+  { loadingProgress : Float
+  , highlightedFeature : Maybe Feature
+  , selectedFeature : Maybe Feature
+  }
+
+
+type Action =
+    Idle
+  | SetLoadingProgress Float
+  | SetHighlightedFeature (Maybe Feature)
+  | SetSelectedFeature (Maybe Feature)
+  | SendHoveredTOID (Maybe String)
+  | SendClickedTOID (Maybe String)
+
+
+type alias Trigger =
+    Signal.Address Action
