a82e14f913ebad2b5fd4ec455b3d0b281ce80977
Miëtek Bak <mietek@bak.io>
2016-01-13 04:33:44 +0000

    Remove unused intersection code

--- src/js/polyline.js
+++ src/js/polyline.js
@@ -40,28 +40,5 @@ const _ = module.exports = {
 
   midpoint: function (ps) {
     return rect.midpoint(_.bounds(0, ps));
-  },
-
-  intersects: function (ps1, ps2) {
-    if (!rect.intersects(_.bounds(0, ps1), _.bounds(0, ps2))) {
-      return false;
-    } else {
-      for (let i = 0; i < ps1.length - 1; i++) {
-        const s1 = {
-          p1: ps1[i],
-          p2: ps1[i + 1]
-        };
-        for (let j = 0; j < ps2.length - 1; j++) {
-          const s2 = {
-            p1: ps2[j],
-            p2: ps2[j + 1]
-          };
-          if (segment.intersects(s1, s2)) {
-            return true;
-          }
-        }
-      }
-      return false;
-    }
   }
 };
