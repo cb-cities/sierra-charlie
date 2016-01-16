43b609ef4c6cc95acc4c1e25f1df6f507d44750a
Miëtek Bak <mietek@bak.io>
2016-01-16 19:40:58 +0000

    Update packaging and dependencies

--- bin/server.js
+++ bin/server.js
@@ -10,31 +10,21 @@ function endsWith(s, t) {
 
 
 var app = express();
-var dirPath = path.dirname(__dirname);
-var outPath = dirPath + "/out/";
-app.get("/", function (req, res) {
-    res.sendFile(outPath + "index.html");
-  });
-app.get("/index.html", function (req, res) {
-    res.sendFile(outPath + "index.html");
-  });
-app.get("/index.js", function (req, res) {
-    res.sendFile(outPath + "index.js");
-  });
-app.get("/index.appcache", function (req, res) {
-    res.sendFile(outPath + "index.appcache");
-  });
-app.use("/json", express.static(dirPath + "/json/", {
-    maxAge: "1h",
-    setHeaders: function (res, path, stat) {
-      if (endsWith(path, ".json")) {
-        res.setHeader("Content-Type", "application/json");
-      } else if (endsWith(path, ".json.gz")) {
-        res.setHeader("Content-Type", "application/json");
-        res.setHeader("Content-Encoding", "gzip");
-      }
+var dir = path.dirname(__dirname);
+
+app.use("/", express.static(dir + "/out"));
+app.use("/json", express.static(dir + "/json", {
+  maxAge: "1h",
+  setHeaders: function (res, path, stat) {
+    if (endsWith(path, ".json")) {
+      res.setHeader("Content-Type", "application/json");
+    } else if (endsWith(path, ".json.gz")) {
+      res.setHeader("Content-Type", "application/json");
+      res.setHeader("Content-Encoding", "gzip");
     }
-  }));
+  }
+}));
+
 app.listen(3000, "0.0.0.0", function (err) {
-    console.log(err ? err : "Listening at http://0.0.0.0:3000");
-  });
+  console.log(err ? err : "Listening at http://0.0.0.0:3000");
+});
