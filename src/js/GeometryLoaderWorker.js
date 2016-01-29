39aac7ecb9d57f54cd9179457eb0e3c7f3955b04
Miëtek Bak <mietek@bak.io>
2016-01-29 04:12:49 +0000

    Load indices from data files

--- src/js/GeometryLoaderWorker.js
+++ src/js/GeometryLoaderWorker.js
@@ -10,11 +10,11 @@ self.onmessage = function (event) {
   switch (data.message) {
     case "startLoading": {
       loader.loadRoadNodes(data.origin);
+      loader.loadAddresses(data.origin);
       for (let i = 1; i <= 4; i++) {
         loader.loadRoadLinks(data.origin, i);
       }
       loader.loadRoads(data.origin);
-      loader.loadAddresses(data.origin);
       break;
     }
   }
