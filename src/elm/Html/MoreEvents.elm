bd23bc231983fe1b08498aa8d9827cd36425d8e3
Carlin Liao <carlin.liao@berkeley.edu>
2017-11-01 18:12:08 -0700

    temp in-progress commit for eval

--- src/elm/Html/MoreEvents.elm
+++ src/elm/Html/MoreEvents.elm
@@ -1,44 +1,32 @@
 module Html.MoreEvents exposing (..)
 
 import Html exposing (Attribute)
-import Html.Events exposing (defaultOptions, on, onWithOptions)
-import Json.Decode as Json exposing (map4, field)
+import Html.Events exposing (defaultOptions, on)
+import Json.Decode as Json exposing (map, field, bool)
 
 
-type alias ModifierKeys =
-    { shiftKey : Bool
-    , ctrlKey : Bool
-    , altKey : Bool
-    , metaKey : Bool
-    }
+-- type alias ModifierKeys =
+--     { shiftKey : Bool
+--     , ctrlKey : Bool
+--     , altKey : Bool
+--     , metaKey : Bool
+--     }
+
+-- compiler complains that i'm returning a `msg` instead of a `Json.Decoder msg`
+modifierKeys : msg -> msg -> msg -> msg -> msg -> Json.Decoder msg
+modifierKeys plainMsg shiftMsg ctrlMsg altMsg metaMsg =
+    if (field "metaKey" bool) then
+        map metaMsg metaMsg
+    else if (field "altKey" bool) then
+        altMsg
+    else if (field "mods.ctrlKey" bool) then
+        ctrlMsg
+    else if (field "mods.shiftKey" bool) then
+        shiftMsg
+    else
+        plainMsg 
 
 
-modifierKeys : Json.Decoder ModifierKeys
-modifierKeys =
-    map4 ModifierKeys
-        (field "shiftKey" Json.bool)
-        (field "ctrlKey" Json.bool)
-        (field "altKey" Json.bool)
-        (field "metaKey" Json.bool)
-
-
-onClickWithModifiers : a -> a -> a -> a -> a -> Attribute Msg
+onClickWithModifiers : msg -> msg -> msg -> msg -> msg -> Attribute msg
 onClickWithModifiers plainMsg shiftMsg ctrlMsg altMsg metaMsg =
-    on "click"
-        modifierKeys
-        (\mods ->
-            let
-                msg =
-                    if mods.metaKey then
-                        metaMsg
-                    else if mods.altKey then
-                        altMsg
-                    else if mods.ctrlKey then
-                        ctrlMsg
-                    else if mods.shiftKey then
-                        shiftMsg
-                    else
-                        plainMsg
-            in
-                Signal.message msg
-        )
+    on "click" (modifierKeys plainMsg shiftMsg ctrlMsg altMsg metaMsg)
